f2231ab40b1d6104ffde8e774aae6625f5d898a7
jdhofmann <justindhofmann@gmail.com>
2020-11-19 20:26:03 -0500

    jerks

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
+
 
 class Quotes extends Component {
+
+renderQuotes = () => {
+    return this.props.quotes.map( quote => <QuoteCard 
+      key={quote.id}
+      quote={quote}
+      removeQuote={this.props.removeQuote}
+      upVote={this.props.upVote}
+      downVote={this.props.downVote}
+    /> )
+  }
 
   render() {
     return (
@@ -15,11 +27,10 @@ class Quotes extends Component {
         <div className="container">
           <div className="row">
             <div className="col-md-4">
-              {/*
-                TODO:
+              {this.renderQuotes()}
+                {/* TODO:
 
-                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes
-               */}
+                Render Quotes With QuoteCard component and pass down callback props for removing, upvoting and downvoting quotes */}
             </div>
           </div>
         </div>
@@ -28,5 +39,18 @@ class Quotes extends Component {
   }
 }
 
+const mdp = dispatch => {
+  return {
+    removeQuote: (quoteId) => dispatch(removeQuote(quoteId)),
+    upVote: (quoteId) => dispatch(upvoteQuote(quoteId)),
+    downVote: (quoteId) => dispatch(downvoteQuote(quoteId))
+  }
+}
+
+const msp = state => {
+  return {
+    quotes: state.quotes
+  }
+}
 //add arguments to connect as needed
-export default connect()(Quotes);
+export default connect(msp, mdp)(Quotes);
--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,49 @@
 export default (state = [], action) => {
-  return state;
+
+  // console.log("reducer is reducing something")
+  let quotes = [...state]
+    let foundQuote = quotes.find(q => q.id === action.quoteId)
+
+  switch (action.type){
+
+    case 'ADD_QUOTE':
+      // console.log("adding quote", action)
+
+      return [...state, action.quote]
+
+    case 'REMOVE_QUOTE':
+      
+      let remainingQuotes = state.filter( q => q.id !== action.quoteId)
+      console.log(remainingQuotes)
+      return remainingQuotes
+
+
+    case 'UPVOTE_QUOTE':
+      console.log("reducer is trying")
+      // let quotes = [...state]
+      // let foundQuote = quotes.find(q => q.id === action.quoteId)
+      foundQuote.votes += 1
+      console.log(quotes, foundQuote)
+      return quotes
+
+
+      case 'DOWNVOTE_QUOTE':
+      if(foundQuote.votes === 0){
+        foundQuote.votes = 0
+      // console.log(quotes, foundQuote)
+      }
+      else {
+        foundQuote.votes -= 1
+      }
+    return quotes
+      
+
+    default:
+      return state
+  }
+
+
+  // return state;
 }
+
+
